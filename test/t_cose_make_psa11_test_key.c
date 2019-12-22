c606914466871d48d779b60f3d6552d9534f661f
Laurence Lundblade <lgl@securitytheory.com>
2019-12-22 11:15:03 -0800

    Remove some extraneous PSA crypto layer files

--- test/t_cose_make_psa11_test_key.c
+++
@@ -1,9 +0,0 @@
-//
-//  t_cose_make_psa11_test_key.c
-//  t_cose_psa_off_target
-//
-//  Created by Laurence Lundblade on 12/19/19.
-//  Copyright © 2019 Laurence Lundblade. All rights reserved.
-//
-
-#include "t_cose_make_psa11_test_key.h"
