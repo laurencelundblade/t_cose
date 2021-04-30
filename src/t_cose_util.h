90055a1a68acea2cbeb664be109163bf9cb9e1b2
Laurence Lundblade <lgl@securitytheory.com>
2021-04-29 20:16:01 -0700

    improve aad test coverage

--- src/t_cose_util.h
+++ src/t_cose_util.h
@@ -71,6 +71,7 @@ int32_t hash_alg_id_from_sig_alg_id(int32_t cose_algorithm_id);
  * \param[in] cose_algorithm_id     The COSE signing algorithm ID. Used to
  *                                  determine which hash function to use.
  * \param[in] protected_parameters  Full, CBOR encoded, protected parameters.
+ * \param[in] aad  Application Authenitcation Data to be included in TBS.
  * \param[in] payload               The CBOR-encoded payload.
  * \param[in] buffer_for_hash       Pointer and length of buffer into which
  *                                  the resulting hash is put.
