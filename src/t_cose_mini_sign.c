b3d9d5cf223fb8805d9654ae3c8ce22e523d434d
Laurence Lundblade <lgl@securitytheory.com>
2022-05-24 12:12:21 -0700

    checkpoint mini sign work

--- src/t_cose_mini_sign.c
+++ src/t_cose_mini_sign.c
@@ -1,5 +1,5 @@
 //
-//  t_cose_austere_sign.c
+//  t_cose_mini_sign.c
 //  t_cose
 //
 //  Created by Laurence Lundblade on 5/2/22.
@@ -10,35 +10,39 @@
 #include "t_cose/t_cose_common.h"
 #include "t_cose_crypto.h"
 
-#include "qcbor/qcbor_encode.h" /* Only uses  the function QCBOREncode_EncodeHead()*/
 #include "qcbor/UsefulBuf.h"
 
-/*
- 1) start the hash function
 
- 2) hash the fixed byte string that is start of the Sig_structure
-
- 3) hash the payload
-
- 4) finish the hash
-
- 5) copy the COSE_Sign1 stuff before the payload to the output.
-
- 6) copy the payload to the output
-
- 7) copy another fixed length string to the output, the bstr head for the signature
-
- 7) call ECDA sign with the hash as input and output directly to the output buffer
- */
+#define T_COSE_MINI_SIGN_SELECT_ES384
 
 
 
-// Adjust these for algorithm and payload length
-#define PAYLOAD_LEN 128 // Only works for lengths > 24 and < 256
-#define COSE_HASH   COSE_ALGORITHM_SHA_384
-#define ENCODED_EC384 0x22 // Make sure this is the right encoding if you change this
-#define ENCODED_EC384_SIG_LEN 96 // Make sure this is the right encoding if you change this
 
+#if defined(T_COSE_MINI_SIGN_SELECT_ES256)
+
+#define MINI_SIGN_HASH           COSE_ALGORITHM_SHA_256
+#define MINI_SIGN_HASH_LEN       T_COSE_CRYPTO_SHA256_SIZE
+#define MINI_SIGN_ALG            T_COSE_ALGORITHM_ES256
+#define MINI_SIGN_ALG_ID_BYTES   0x26 /* The literal byte that appears in the CBOR encoding */
+#define MINI_SIGN_SIG_LEN        T_COSE_EC_P256_SIG_SIZE // Code below only works for lengths < 256
+
+#elif defined(T_COSE_MINI_SIGN_SELECT_ES384)
+
+#define MINI_SIGN_HASH           COSE_ALGORITHM_SHA_384
+#define MINI_SIGN_HASH_LEN       T_COSE_CRYPTO_SHA384_SIZE
+#define MINI_SIGN_ALG            T_COSE_ALGORITHM_ES384
+#define MINI_SIGN_ALG_ID_BYTES   0x38, 0x22 /* The literal byte that appears in the CBOR encoding */
+#define MINI_SIGN_SIG_LEN        T_COSE_EC_P384_SIG_SIZE // Code below only works for lengths < 256
+
+#elif defined(T_COSE_MINI_SIGN_SELECT_ES512)
+
+#define MINI_SIGN_HASH           COSE_ALGORITHM_SHA_512
+#define MINI_SIGN_HASH_LEN       T_COSE_CRYPTO_SHA512_SIZE
+#define MINI_SIGN_ALG            T_COSE_ALGORITHM_ES521
+#define MINI_SIGN_ALG_ID_BYTES   0x38, 0x23 /* The literal byte that appears in the CBOR encoding */
+#define MINI_SIGN_SIG_LEN        T_COSE_EC_P512_SIG_SIZE // Code below only works for lengths < 256
+
+#endif
 
 /* This has all sort of stuff packed in and hard coded, including the payload length.
 
@@ -54,30 +58,35 @@
 
 static uint8_t start_sig_struct[] = {
     0x84,
-    0x6A,
-    'S', 'i', 'g', 'n', 'a', 't', 'u', 'r', 'e', '1',
-    0x44, 0xA1, 0x01, 0x38, ENCODED_EC384, // bstr wrapped protected header wtih algorithm ID
+    0x6A,'S', 'i', 'g', 'n', 'a', 't', 'u', 'r', 'e', '1',
+    0x44, 0xA1, 0x01, MINI_SIGN_ALG_ID_BYTES, // bstr wrapped protected header wtih algorithm ID
     0x40, // Empty bstr for aad
 };
 
 static uint8_t start_cose_sign1[] = {
     0x84,
-    0x44, 0xA1, 0x01, 0x38, ENCODED_EC384, // bstr wrapped protected header wtih algorithm ID
+    0x44, 0xA1, 0x01, MINI_SIGN_ALG_ID_BYTES, // bstr wrapped protected header wtih algorithm ID
     0xa0, // no unprotected headers, put some here if you want
 };
 
 static uint8_t cose_sign1_sig_start[] = {
-    0x58, ENCODED_EC384_SIG_LEN
+    0x58, MINI_SIGN_SIG_LEN
 };
 
 
-/* Len > 24 and < 65355 */
+
+
+/* This maximum is for a CBOR head for a byte string no longer than
+ * UINT16_MAX, not the general case for a CBOR head. */
+#define MAX_CBOR_HEAD 3
+
+/* Len < 65355 */
 static inline struct q_useful_buf_c
-EncodeBstrHead(struct q_useful_buf b, size_t len)
+encode_bstr_head(struct q_useful_buf b, size_t len)
 {
     uint8_t *bb = b.ptr;
 
-    if(b.len < 3) {
+    if(b.len < MAX_CBOR_HEAD) {
         return NULLUsefulBufC;
     }
 
@@ -99,52 +108,6 @@ EncodeBstrHead(struct q_useful_buf b, size_t len)
 }
 
 
-typedef struct {
-    UsefulBuf Storage;
-    size_t uPosition;
-} SAP;
-
-static inline void
-init(SAP *pOut, struct q_useful_buf o)
-{
-    pOut->Storage = o;
-}
-
-static inline void
-simple_append(SAP *pOut, const UsefulBufC data)
-{
-    if(pOut->Storage.len - pOut->uPosition < data.len) {
-        return;
-    }
-
-    memcpy((uint8_t *)(pOut->Storage.ptr) + pOut->uPosition, data.ptr, data.len);
-
-    pOut->uPosition += data.len;
-}
-
-static inline struct q_useful_buf
-get_place(SAP *pOut)
-{
-    return (struct q_useful_buf){(uint8_t *)(pOut->Storage.ptr) + pOut->uPosition,
-        pOut->Storage.len - pOut->uPosition};
-}
-
-
-static inline void advance(SAP *pOut, size_t len)
-{
-    pOut->uPosition += len;
-}
-
-static inline struct q_useful_buf_c
-geto(SAP *pOut)
-{
-    return (struct q_useful_buf_c){pOut->Storage.ptr, pOut->Storage.len};
-}
-
-
-
-
-
 
 
 enum t_cose_err_t
@@ -153,23 +116,26 @@ t_cose_mini_sign(const struct q_useful_buf_c payload,
                  const struct q_useful_buf   output_buffer,
                  struct q_useful_buf_c      *output)
 {
-
     struct t_cose_crypto_hash  hash_ctx;
     enum t_cose_err_t          err;
     struct q_useful_buf_c      computed_hash;
     struct q_useful_buf_c      signature;
-    MakeUsefulBufOnStack(      hash_output, 64); // TODO: correct size for selected hash
-    MakeUsefulBufOnStack(      payload_head_buffer, QCBOR_HEAD_BUFFER_SIZE);
+    MakeUsefulBufOnStack(      hash_output, MINI_SIGN_HASH_LEN);
+    MakeUsefulBufOnStack(      payload_head_buffer, MAX_CBOR_HEAD);
     struct q_useful_buf_c      payload_head;
     struct q_useful_buf        tmp;
 
-    payload_head = EncodeBstrHead(payload_head_buffer, payload.len);
+    payload_head = encode_bstr_head(payload_head_buffer, payload.len);
+
+    if(payload_head.ptr == NULL) {
+        return 99; // TODO: error code
+    }
 
 
     /* --- hash the Sig_structure --- */
     /* Don't actually have to create the Sig_structure fully in
      * memory. Just have to compute the hash of it. */
-    err = t_cose_crypto_hash_start(&hash_ctx, COSE_ALGORITHM_SHA_384);
+    err = t_cose_crypto_hash_start(&hash_ctx, MINI_SIGN_HASH);
 
     t_cose_crypto_hash_update(&hash_ctx, UsefulBuf_FROM_BYTE_ARRAY_LITERAL(start_sig_struct));
     t_cose_crypto_hash_update(&hash_ctx, payload_head);
@@ -178,7 +144,12 @@ t_cose_mini_sign(const struct q_useful_buf_c payload,
     err = t_cose_crypto_hash_finish(&hash_ctx, hash_output, &computed_hash);
 
 
-    if(output_buffer.len < payload.len + 4 + sizeof(start_cose_sign1) + sizeof(cose_sign1_sig_start)) {
+    const size_t required_len = payload.len +
+                                MAX_CBOR_HEAD +
+                                sizeof(start_cose_sign1) +
+                                sizeof(cose_sign1_sig_start);
+
+    if(output_buffer.len < required_len) {
         return 0;
     }
 
@@ -203,115 +174,16 @@ t_cose_mini_sign(const struct q_useful_buf_c payload,
     tmp.ptr = p;
 
 
-    t_cose_crypto_sign(T_COSE_ALGORITHM_ES384,
+    t_cose_crypto_sign(MINI_SIGN_ALG,
                        signing_key,
                        computed_hash,
                        tmp,
                        &signature);
 
     output->ptr = output_buffer.ptr;
-    output->len = tmp2.len + ENCODED_EC384_SIG_LEN;
+    output->len = tmp2.len + MINI_SIGN_SIG_LEN;
 
 
     return 0;
 }
 
-int foo()
-{
-    return 99;
-}
-
-
-#ifdef NODEF
-#if 0
-/*
-    SAP UBO;
-
-    init(&UBO, output_buffer);
-
-    simple_append(&UBO, Q_USEFUL_BUF_FROM_BYTE_ARRAY_LITERAL(start_cose_sign1));
-    simple_append(&UBO, payload_head);
-    simple_append(&UBO, payload);
-    simple_append(&UBO, Q_USEFUL_BUF_FROM_BYTE_ARRAY_LITERAL(cose_sign1_sig_start));
-
-    // Output the signature directly into the output buffer.
-    tmp = get_place(&UBO);
-
-    t_cose_crypto_sign(T_COSE_ALGORITHM_ES384,
-                       signing_key,
-                       computed_hash,
-                       tmp,
-                       &signature);
-
-    advance(&UBO, signature.len);
-
-    *output = geto(&UBO);*/
-
-#else
-
-
-    UsefulOutBuf UBO;
-
-    UsefulOutBuf_Init(&UBO, output_buffer);
-
-    UsefulOutBuf_AppendUsefulBuf(&UBO, Q_USEFUL_BUF_FROM_BYTE_ARRAY_LITERAL(start_cose_sign1));
-    UsefulOutBuf_AppendUsefulBuf(&UBO, payload_head);
-    UsefulOutBuf_AppendUsefulBuf(&UBO, payload);
-    UsefulOutBuf_AppendUsefulBuf(&UBO, Q_USEFUL_BUF_FROM_BYTE_ARRAY_LITERAL(cose_sign1_sig_start));
-
-    /* Output the signature directly into the output buffer. */
-    tmp = UsefulOutBuf_GetOutPlace(&UBO);
-
-    t_cose_crypto_sign(T_COSE_ALGORITHM_ES384,
-                       signing_key,
-                       computed_hash,
-                       tmp,
-                       &signature);
-
-    UsefulOutBuf_Advance(&UBO, signature.len);
-
-    *output = UsefulOutBuf_OutUBuf(&UBO);
-
-
-
-//#else
-    size_t cose_sign1_len;
-    /* --- Copy first part of COSE_Sign1 to output buffer -- */
-    cose_sign1_len = 0;
-
-    const struct q_useful_buf_c xx = Q_USEFUL_BUF_FROM_BYTE_ARRAY_LITERAL(start_cose_sign1);
-    q_useful_buf_copy(output_buffer,
-                      UsefulBuf_FROM_BYTE_ARRAY_LITERAL(start_cose_sign1));
-    cose_sign1_len += xx.len;
-
-
-    useful_buf_copy_offset(output_buffer, cose_sign1_len, payload_head);
-    cose_sign1_len += payload_head.len;
-
-
-    /* -- Copy the payload to the output buffer -- */
-    useful_buf_copy_offset(output_buffer, cose_sign1_len, payload);
-    cose_sign1_len += payload.len;
-
-    /* -- Do the signing and put it in the output buffer */
-    const struct q_useful_buf_c yy = UsefulBuf_FROM_BYTE_ARRAY_LITERAL(cose_sign1_sig_start);
-    useful_buf_copy_offset(output_buffer, cose_sign1_len, yy);
-    cose_sign1_len += yy.len;
-
-    tmp = (struct q_useful_buf){(uint8_t *)output_buffer.ptr + cose_sign1_len,
-                               output_buffer.len - cose_sign1_len};
-
-
-    t_cose_crypto_sign(T_COSE_ALGORITHM_ES384,
-                       signing_key,
-                       computed_hash,
-                       tmp,
-                       &signature);
-
-    cose_sign1_len += signature.len;
-
-    output->ptr = output_buffer.ptr;
-    output->len = cose_sign1_len;
-
-#endif
-#endif
