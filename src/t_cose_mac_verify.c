fdf9edfd82f054e290bda46ab0993fa9ea3d7a9d
Adam Kulesza <adam.kule@gmail.com>
2022-08-03 11:56:57 +0200

    Changes parameters file mentions to public interface
    Signed-off-by: Adam Kulesza <adam.kulesza@arm.com>
    Change-Id: Icd213d0759170e4ab9bcde3595496a7990fb7b68

--- src/t_cose_mac_verify.c
+++ src/t_cose_mac_verify.c
@@ -11,7 +11,7 @@
 #include "qcbor/qcbor_spiffy_decode.h"
 #include "t_cose_crypto.h"
 #include "t_cose/t_cose_mac_verify.h"
-#include "t_cose_parameters.h"
+#include "t_cose/t_cose_parameters.h"
 #include "t_cose_util.h"
 
 #ifndef T_COSE_DISABLE_MAC0
