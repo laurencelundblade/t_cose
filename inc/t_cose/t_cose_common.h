d113dc9e450d9997c097a8880df450f0a2b8470d
Adam Kulesza <adam.kule@gmail.com>
2022-08-03 11:59:46 +0200

    Moves function from common to util
    Signed-off-by: Adam Kulesza <adam.kule@gmail.com>
    Change-Id: I3763605994b32a9e667773798452479e1612f967

--- inc/t_cose/t_cose_common.h
+++ inc/t_cose/t_cose_common.h
@@ -14,7 +14,7 @@
 #define __T_COSE_COMMON_H__
 
 #include <stdint.h>
-#include <qcbor/qcbor.h>
+#include "t_cose/q_useful_buf.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -667,29 +667,6 @@ struct t_cose_parameters {
  *
  */
 #define T_COSE_OPT_DECODE_ONLY  0x00000008
-
-/**
- * \brief Map QCBOR decode error to COSE errors.
- *
- * \param[in] qcbor_error   The QCBOR error to map.
- *
- * \return This returns one of the error codes defined by
- *         \ref t_cose_err_t.
- */
-static inline enum t_cose_err_t
-qcbor_decode_error_to_t_cose_error(QCBORError qcbor_error)
-{
-    if(qcbor_error == QCBOR_ERR_TOO_MANY_TAGS) {
-        return T_COSE_ERR_TOO_MANY_TAGS;
-    }
-    if(QCBORDecode_IsNotWellFormedError(qcbor_error)) {
-        return T_COSE_ERR_CBOR_NOT_WELL_FORMED;
-    }
-    if(qcbor_error != QCBOR_SUCCESS) {
-        return T_COSE_ERR_SIGN1_FORMAT;
-    }
-    return T_COSE_SUCCESS;
-}
 
 #ifdef __cplusplus
 }
