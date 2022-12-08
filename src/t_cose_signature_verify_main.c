4feb29b42e021f571d465063bf0de56d8c33eed8
Laurence Lundblade <lgl@securitytheory.com>
2022-12-07 22:10:35 -0700

    Clean up signer/verify interface

--- src/t_cose_signature_verify_main.c
+++ src/t_cose_signature_verify_main.c
@@ -18,17 +18,15 @@
 #include "t_cose_crypto.h"
 
 
-/* Warning: this is still early development. Documentation may be incorrect. */
-
 /*
- * This is an implementation of t_cose_signature_verify1_callback.
+ * This is an implementation of \ref t_cose_signature_verify1_cb.
  */
 static enum t_cose_err_t
-t_cose_signature_verify1_main(struct t_cose_signature_verify   *me_x,
-                               const uint32_t                   option_flags,
-                               const struct t_cose_sign_inputs *sign_inputs,
-                               const struct t_cose_parameter   *parameter_list,
-                               const struct q_useful_buf_c      signature)
+t_cose_signature_verify1_main_cb(struct t_cose_signature_verify   *me_x,
+                                 const uint32_t                   option_flags,
+                                 const struct t_cose_sign_inputs *sign_inputs,
+                                 const struct t_cose_parameter   *parameter_list,
+                                 const struct q_useful_buf_c      signature)
 {
     const struct t_cose_signature_verify_main *me =
                           (const struct t_cose_signature_verify_main *)me_x;
@@ -89,16 +87,16 @@ Done:
           Signature validate
           Signature didn't validate
 
- This is an implementation of t_cose_signature_verify_callback
+ This is an implementation of t_cose_signature_verify_cb
  */
 static enum t_cose_err_t
-t_cose_signature_verify_main(struct t_cose_signature_verify     *me_x,
-                              const uint32_t                      option_flags,
-                              const struct t_cose_header_location loc,
-                              const struct t_cose_sign_inputs    *sign_inputs,
-                              struct t_cose_parameter_storage    *param_storage,
-                              QCBORDecodeContext                 *qcbor_decoder,
-                              struct t_cose_parameter           **decoded_signature_parameters)
+t_cose_signature_verify_main_cb(struct t_cose_signature_verify     *me_x,
+                                const uint32_t                      option_flags,
+                                const struct t_cose_header_location loc,
+                                const struct t_cose_sign_inputs    *sign_inputs,
+                                struct t_cose_parameter_storage    *param_storage,
+                                QCBORDecodeContext                 *qcbor_decoder,
+                                struct t_cose_parameter           **decoded_signature_parameters)
 {
     const struct t_cose_signature_verify_main *me =
                             (const struct t_cose_signature_verify_main *)me_x;
@@ -133,11 +131,11 @@ t_cose_signature_verify_main(struct t_cose_signature_verify     *me_x,
     /* --- Done decoding the COSE_Signature --- */
 
 
-    return_value = t_cose_signature_verify1_main(me_x,
-                                                 option_flags,
-                                                 sign_inputs,
-                                                *decoded_signature_parameters,
-                                                 signature);
+    return_value = t_cose_signature_verify1_main_cb(me_x,
+                                                    option_flags,
+                                                    sign_inputs,
+                                                   *decoded_signature_parameters,
+                                                    signature);
 Done:
     return return_value;
 }
@@ -147,6 +145,6 @@ void
 t_cose_signature_verify_main_init(struct t_cose_signature_verify_main *me)
 {
     memset(me, 0, sizeof(*me));
-    me->s.callback  = t_cose_signature_verify_main;
-    me->s.callback1 = t_cose_signature_verify1_main;
+    me->s.verify_cb  = t_cose_signature_verify_main_cb;
+    me->s.verify1_cb = t_cose_signature_verify1_main_cb;
 }
